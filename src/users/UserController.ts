6454ba299d71533192cf953ef41651b96e02417f
shivani sharma <shikha@shivanis-MacBook-Air.local>
2024-04-20 17:39:19 -0400

    user routes added

--- src/users/UserController.ts
+++ src/users/UserController.ts
@@ -30,28 +30,46 @@ export const confirmEmail = async (req: Request, res: Response) => {
 };
 
 export const updateUser = async (req: Request, res: Response) => {
-  try {
-    const createdUser = await userServices.register(req.body);
-    res.status(200).send(createdUser);
-  } catch (error) {
-    return res.status(500).send(getErrorMessage(error));
-  }
+  const { userId } = req.params;
+    try {
+      const updatedUser = await userServices.updateUser(userId,req.body);
+      res.status(200).send(updatedUser);
+    } catch (error) {
+      return res.status(500).send(getErrorMessage(error));
+    }
 };
 
+
+
 export const getAllUsers = async (req: Request, res: Response) => {
-  try {
-    const createdUser = await userServices.register(req.body);
-    res.status(200).send(createdUser);
-  } catch (error) {
-    return res.status(500).send(getErrorMessage(error));
+    
+  try{
+    const users = await userServices.getAllUsers();
+    res.status(200).send(users);
+  } catch(error){
+      return res.status(500).send(getErrorMessage(error));
   }
+  
 };
 
 export const getUserById = async (req: Request, res: Response) => {
-  try {
-    const createdUser = await userServices.register(req.body);
-    res.status(200).send(createdUser);
-  } catch (error) {
-    return res.status(500).send(getErrorMessage(error));
+  const { userId } = req.params;
+  try{
+    const user = await userServices.getUserById(userId);
+    res.status(200).send(user);
+  } catch(error){
+      return res.status(500).send(getErrorMessage(error));
+  } 
+};
+
+
+export const deleteUser = async (req:Request, res:Response) => {
+  const { userId } = req.params;
+  try{
+    const status = await userServices.deleteUser(userId);
+    res.status(200).send(status);
+  } catch(error){
+      return res.status(500).send(getErrorMessage(error));
   }
+  
 };
