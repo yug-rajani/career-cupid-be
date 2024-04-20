d6c1a7bf4f6dc9581444c07fad3eac130315cead
shivani sharma <shikha@shivanis-MacBook-Air.local>
2024-04-20 17:40:39 -0400

    job posting routes added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,8 +3,10 @@
  */
 import express from 'express';
 import UserRoutes from './UserRoutes';
+import JobPostingRoutes from './JobPostingRoutes'
 
 export const routes = express.Router();
 
 
 routes.use(UserRoutes);
+routes.use(JobPostingRoutes);
